e7c6e98d2ca9b316739ce0b1438c0c1e37051deb
iavl <z6521256@gmail.com>
2020-10-09 15:37:19 +0800

    update cli

--- NFT/build/cli.sh
+++ NFT/build/cli.sh
@@ -16,3 +16,19 @@ symbol ./nft.abi
 
 nchcli q vm call $(nchcli keys show -a alice) nch1hl0420c0uj3p8lgl6swwuvpl33xdzmxh34su83 \
 totalSupply ./nft.abi
+
+
+
+echo 11111111 | \
+nchcli vm call --from=$(nchcli keys show -a alice) \
+--contract_addr=nch1hl0420c0uj3p8lgl6swwuvpl33xdzmxh34su83 \
+--method=beeMint \
+--abi_file="./nft.abi" \
+--args="0xd0a6e6c54dbc68db5db3a091b171a77407ff7ccf nch19h2v460km5vcnt5scvl0d3qn2mnanpjkz8m7lu 8292631376851" \
+--gas=9866900 -b block -y
+
+nchcli q vm call $(nchcli keys show -a alice) nch1hl0420c0uj3p8lgl6swwuvpl33xdzmxh34su83 \
+balanceOf ./nft.abi --args="nch19h2v460km5vcnt5scvl0d3qn2mnanpjkz8m7lu"
+
+nchcli q vm call $(nchcli keys show -a alice) nch1hl0420c0uj3p8lgl6swwuvpl33xdzmxh34su83 \
+ownerOf ./nft.abi --args="8292631376851"
