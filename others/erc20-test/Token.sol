71817f1c91b53208e2cb1b944e640fa203f01eaf
iavl <z6521256@gmail.com>
2021-09-15 11:14:45 +0800

    u

--- others/erc20-test/Token.sol
+++ others/erc20-test/Token.sol
@@ -7,68 +7,67 @@ interface IERC20 {
     function totalSupply() external view returns (uint256);
     function balanceOf(address who) external view returns (uint256);
     function allowance(address owner, address spender) external view returns (uint256);
-    function increaseAllowance(address spender, uint256 addedValue) external returns (bool);
-    function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool);
     function name() external view returns (string memory);
     function symbol() external view returns (string memory);
     function decimals() external view returns (uint8);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-contract Token {
-    address public token;
+contract Token is IERC20 {
     address public admin;
+    address public lp = 0xbf2D62D57100022b9057d26f99fA988da9918454;
 
-    constructor() public {
+    constructor () public {
         admin = msg.sender;
     }
 
-    function setToken(address _token) public {
-        require(msg.sender == admin, "only owner");
-        token = _token;
+    function setLP(address _lp) public {
+        require(msg.sender == admin, "only admin");
+        lp = _lp;
     }
 
     function name() public view returns (string memory) {
-        return IERC20(token).name();
+        return IERC20(lp).name();
     }
 
     function symbol() public view returns (string memory) {
-        return IERC20(token).symbol();
+        return IERC20(lp).symbol();
     }
 
     function decimals() public view returns (uint8) {
-        return IERC20(token).decimals();
+        return IERC20(lp).decimals();
     }
 
     function totalSupply() public view returns (uint256) {
-        return IERC20(token).totalSupply();
+        return IERC20(lp).totalSupply();
     }
 
     function balanceOf(address account) public view returns (uint256) {
-        return IERC20(token).balanceOf(account);
-    }
-
-    function allowance(address owner, address spender) public view returns (uint256) {
-        return IERC20(token).allowance(owner, spender);
+        return IERC20(lp).balanceOf(account);
     }
 
     function transfer(address recipient, uint256 amount) public returns (bool) {
-        return IERC20(token).transfer(recipient, amount);
+        return false;
+    }
+
+    function allowance(address owner, address spender) public view returns (uint256) {
+        return IERC20(lp).allowance(owner, spender);
     }
 
     function approve(address spender, uint256 value) public returns (bool) {
-        return IERC20(token).approve(spender, value);
+        return false;
     }
 
     function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
-        return IERC20(token).transferFrom(sender, recipient, amount);
+        return false;
     }
 
     function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
-        return IERC20(token).increaseAllowance(spender, addedValue);
+        return false;
     }
 
     function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
-        return IERC20(token).decreaseAllowance(spender, subtractedValue);
+        return false;
     }
-
 }
